bd7b901872c3034a4eb673aac4830aff5d141c66
Jose Garza <90730468+SVENTRIPIKAL@users.noreply.github.com>
2022-06-05 11:05:31 -0500

    EXERCISES #1-11
    -completed additional exercises
    -rewrote Warehouse.next() function

--- EXERCISES/NUMBERS/Main.java
+++ EXERCISES/NUMBERS/Main.java
@@ -1,28 +1,48 @@
 package NUMBERS;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.text.NumberFormat;
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        //#3    ARRAY OF NAMES
         for (int i = 0; i < 13; i++) {
             if (i % 3 == 0) { System.out.println(); }
             System.out.printf("%s\t\t",Names.next());
             Names.increment();
         } System.out.printf("%n%n");
-        
-        for (int i = 0; i < 366; i++) {
+    
+        //#4    WAREHOUSE ITEM SHELF-LIFE CODES
+        for (int i = 1; i <= 366; i++) {
             System.out.printf("""
                 Code: %s    Days: %s
                 """,
                 Warehouse.next(i), i
             );
         } System.out.println();
-        
+    
+        //#5    SUM OF 10 RANDOM NUMBERS
         for (int i = 0; i < 12; i++) {
             System.out.println(Rndm.next());
             Rndm.increment();
         } System.out.println();
+    
+        //#6    MONEY FORMATTER
+        System.out.printf("%s%n%n", formatMoney("149.32"));
+    
+        //#7    BIG DECIMAL MONEY CALCULATION
+        System.out.printf("%s%n%n", calculateMoney("$12,345.83"));
+    
+        //#8-10 FORMATTING AN INPUT/OUTPUT TABLE
+        printInputOutputTable();
+    
+        //#11   RETURN SUM OF TWO STRING NUMBERS
+        System.out.println(sumOfStringNums("37", "13"));
     }
+    
     
     //#3    ARRAY OF NAMES
     private static final class Names {
@@ -49,13 +69,8 @@ public class Main {
         private Warehouse() {    }
         
         public static int next(int age) {
-            int x;
-            if      (age <= 89)     { x = 0; }
-            else if (age <= 179)    { x = 1; }
-            else if (age <= 269)    { x = 2; }
-            else if (age <= 364)    { x = 3; }
-            else                    { x = -1;}
-            return x;
+            int code = age / 90;
+            return Math.min(code, 3);
         }
     }
     
@@ -77,5 +92,58 @@ public class Main {
         public static void increment() {
             count = count == 10 ? count : count+1;
         }
+    }
+    
+    //#6    MONEY FORMATTER
+    public static String formatMoney(String money) {
+        NumberFormat nf = NumberFormat.getCurrencyInstance();
+        return nf.format(Double.valueOf(money));
+    }
+    
+    //#7    BIG DECIMAL MONEY CALCULATION
+    public static String calculateMoney(String money) throws Exception {
+        NumberFormat nf = NumberFormat.getCurrencyInstance();
+        MathContext mc = new MathContext(5);
+        
+        String parsed = String.valueOf(nf.parse(money));
+        BigDecimal bd = new BigDecimal(parsed).divide(
+                        new BigDecimal("32.19"), mc
+        );
+        return nf.format(Double.valueOf(String.valueOf(bd)));
+    }
+    
+    //#8-10 FORMATTING AN INPUT/OUTPUT TABLE
+    public static void printInputOutputTable() {
+        DecimalFormat moneyFormat = new DecimalFormat("$#,###.##;(#)");
+        DecimalFormat decimalFormat = new DecimalFormat("#,###.#;-#");
+        DecimalFormat leadingFormat = new DecimalFormat("0000000000");
+        DecimalFormat scientificFormat = new DecimalFormat("0.######E00f");
+        String[] input = {
+                "123456.783", "-9876.32532", "23.19283928394829182",
+                "123456",   "-9876.35532"
+        };
+        System.out.printf("""
+            [INPUTS]                            [OUTPUTS]
+            %-10s   %24s        $%,(.2f
+            %-10s   %22s         %,(.2f
+            %-10s   %15s       %.6ef
+            %-10s   %23s         %010d
+            %-10s   %20s           %,.1f%n
+            """, input[0], moneyFormat.format(123456.783),
+            Double.valueOf(input[0]),
+            input[1], moneyFormat.format(-9876.32532),
+            Double.valueOf(input[1]),
+            input[2], scientificFormat.format(23.19283928394829182),
+            Double.valueOf(input[2]),
+            input[3], leadingFormat.format(123456),
+            Integer.parseInt(input[3]),
+            input[4], decimalFormat.format(-9876.35532),
+            Double.valueOf(input[4])
+        );
+    }
+    
+    //#11   RETURN SUM OF TWO STRING NUMBERS
+    public static int sumOfStringNums(String num1, String num2) {
+        return Integer.parseInt(num1) + Integer.parseInt(num2);
     }
 }
